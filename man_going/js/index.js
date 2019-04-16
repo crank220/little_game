3efc04b864ba557fd4bcfbdc665378688e42dbee
shizhiqiang <icrank@qq.com>
2019-04-16 14:34:42 +0800

    review man going

--- man_going/js/index.js
+++ man_going/js/index.js
@@ -1,54 +1,54 @@
-function Spirt(x,y){
-	this.x=x;
-	this.y=y;
-	this.left=function(){
-		game.style.backgroundPositionY="-128px";
-		this.x=this.x-15;
+function Spirt(x, y) {
+	this.x = x;
+	this.y = y;
+	this.left = function () {
+		game.style.backgroundPositionY = "-128px";
+		this.x = this.x - 15;
 		this.show();
 	};
-	this.up=function(){
-		game.style.backgroundPositionY="-384px";
-		this.y=this.y-15;
+	this.up = function () {
+		game.style.backgroundPositionY = "-384px";
+		this.y = this.y - 15;
 		this.show();
 	};
-	this.right=function(){
-		game.style.backgroundPositionY="-256px";
-		this.x=this.x+15;
+	this.right = function () {
+		game.style.backgroundPositionY = "-256px";
+		this.x = this.x + 15;
 		this.show();
 	};
-	this.down=function(){
-		game.style.backgroundPositionY="0px";
-		this.y=this.y+15;
+	this.down = function () {
+		game.style.backgroundPositionY = "0px";
+		this.y = this.y + 15;
 		this.show();
 	};
-	var n=0; // 记录播放到第几张图了
-	this.show=function(){
-		game.style.backgroundPositionX=n+"px";
-		game.style.top = this.y +"px";
-		game.style.left = this.x +"px";
-		n=n-85;
-		if(n<-255){
-			n=0;
+	var n = 0; // 记录播放到第几张图了
+	this.show = function () {
+		game.style.backgroundPositionX = n + "px";
+		game.style.top = this.y + "px";
+		game.style.left = this.x + "px";
+		n = n - 85;
+		if (n < -255) {
+			n = 0;
 		}
 	}
 }
 
-var a=new Spirt(0,0); // 建立一个对象
+var a = new Spirt(0, 0); // 建立一个对象
 // 网页加键盘事件
-document.onkeydown=function(){
-	var k=event.keyCode;
-	switch(k){
+document.onkeydown = function () {
+	var k = event.keyCode;
+	switch (k) {
 		case 37:
 			a.left();
-		break;
+			break;
 		case 38:
 			a.up();
-		break;
+			break;
 		case 39:
 			a.right();
-		break;
+			break;
 		case 40:
 			a.down();
-		break;
+			break;
 	}
 }
